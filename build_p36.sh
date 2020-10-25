cdd7ab72fe419740719a8bea57f6144633c8e874
KaiL4eK <darklexi494@gmail.com>
2020-10-26 01:12:15 +0300

    Disabled install directory

--- build_p36.sh
+++ build_p36.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 catkin config -DPYTHON_EXECUTABLE=$(which python) -DPYTHON_INCLUDE_DIR=$(python -c "from sysconfig import get_paths as gp; print(gp()['include'])")
-catkin config --install
+#catkin config --install
 catkin build cv_bridge
